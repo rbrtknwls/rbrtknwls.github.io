05168b541db706b8147d3db18591cf46e2d218f9
robbie.knowles <robbie.knowles@ucc.on.ca>
2019-02-08 02:05:31 -0500

    APPPPAAAAA
    Need to add more data lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,72 @@
-/*
-	Epilogue by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
 
-(function($) {
+function main() {
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
+(function () {
+   'use strict';
+   
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+	// affix the navbar after scroll below header
+
+
+	
+	// Hide nav on click
+
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+	
+	  	
+    // CounterUp
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){	
+			$('span.count').counterUp({
+					delay: 10, // the delay time in ms
+			time: 1500 // the speed time in ms
+			});
+		}
 	});
+	
+  	// Pretty Photo
 
-	$(function() {
 
-		var	$window = $(window),
-			$body = $('body');
+}());
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Items.
-			$('.item').each(function() {
-
-				var $this = $(this),
-					$header = $this.find('header'),
-					$a = $header.find('a'),
-					$img = $header.find('img');
-
-				// Set background.
-					$a.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Remove original image.
-					$img.remove();
-
-			});
-
-	});
-
-})(jQuery);
+}
+main();
