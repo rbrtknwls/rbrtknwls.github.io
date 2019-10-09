cd20a0cd16b00a65d24faec3eccd7b7aaa111ce2
Robert Knowles <robbie.knowles@ucc.on.ca>
2019-10-09 17:53:50 -0400

    Almost Done?!?!
    Nope
    
    Made alot of good work though

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+var text = ["Websites", "Applets", "Solutions", "Registration Systems", "Data Parsers", "Calculators", "Games", "Simulations"];
+var counter = 1;
+var elem = document.getElementById("fluxtext");
+var inst = setInterval(change, 3000);
+
+function change() {
+  elem.innerHTML = " " + text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
